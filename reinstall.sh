c6f08b97412e0153b656ef813cc2d0ee93490069
James R Qualls <Qualls.James@gmail.com>
2013-09-06 01:24:58 -0400

    Modified for OSX 10.8.4
    uses default elasticsearch installed with `brew install elasticsearch`

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,9 @@
-ES=/usr/share/elasticsearch
+# Modified For OSX 10.8.4 with ES installed with
+# brew install elasticsearch
+ES=/usr/local/Cellar/elasticsearch/0.90.3
 sudo $ES/bin/plugin remove reindex
 mvn -DskipTests clean package
 FILE=`ls ./target/elasticsearch-*zip`
 sudo $ES/bin/plugin -url file:$FILE -install reindex
-sudo service elasticsearch restart
+launchctl stop homebrew.mxcl.elasticsearch
+launchctl start homebrew.mxcl.elasticsearch
